40bf4fd514a97c57a66a20d1be0b17439a150592
Dan Bauman <dan@bauman.space>
2018-05-03 10:44:43 -0500

    remove non-required py2 import

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from setuptools import setup
-VERSION = "0.0.3.3"
+VERSION = "0.0.3.4"
 setup(
     name='dict-to-protobuf',
     description='A teeny Python library for creating protobuf dicts from '
