0536f7dbcbd072c2f67bfbebd732df46e9ad417e
Dan Bauman <dan@bauman.space>
2018-07-02 12:58:28 -0500

    roll release

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from setuptools import setup
-VERSION = "0.0.3.8"
+VERSION = "0.0.3.9"
 setup(
     name='dict-to-protobuf',
     description='A teeny Python library for creating protobuf dicts from '
